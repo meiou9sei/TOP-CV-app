4f140143d53d19927445ed186c0473216ded152a
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-13 12:42:54 -0500

    🚧 Create basic toggle display component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import CVCreator from "./components/CVCreator/CVCreator";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import ToggleDisplay from "./components/ToggleDisplay";
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <ToggleDisplay />
       <CVCreator />
       <Footer />
     </div>
