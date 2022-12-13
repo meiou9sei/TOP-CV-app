0d05f0b4f9848890d0aeb38949ea72cd9dd19a17
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-13 13:09:52 -0500

    🚧 Create basic HTML for Work Experience Input

--- src/components/CVCreator/CVCreator.js
+++ src/components/CVCreator/CVCreator.js
@@ -1,11 +1,13 @@
 import React from "react";
 import InfoEducation from "./InfoEducation";
+import InfoExperience from "./InfoExperience";
 import InfoGeneral from "./InfoGeneral";
 
 export default function CVCreator() {
   return (
     <form className='CVCreatorForm'>
       <InfoGeneral />
+      <InfoExperience />
       <InfoEducation />
     </form>
   );
