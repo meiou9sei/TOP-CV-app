0941c35e2607e4a7c661e6a375ec845849e1c343
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-13 13:17:14 -0500

    🚧 create add/delete info section component

---
+++ src/components/CVCreator/AddDeleteInfoSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AddInfoSection() {
+  return (
+    <div className='add-delete-info-section-buttons'>
+      <button className='add-info-section-button'>Add Another Position</button>
+      <button className='delete-info-section-button'>
+        Delete Last Position
+      </button>
+    </div>
+  );
+}
