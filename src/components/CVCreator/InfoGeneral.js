0a08db0a049d928f546e5b7b835db400792df010
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-17 16:51:34 -0500

    🔥 remove required tag on inputs

--- src/components/CVCreator/InfoGeneral.js
+++ src/components/CVCreator/InfoGeneral.js
@@ -7,32 +7,32 @@ export default function InfoGeneral({ infoGeneral, setInfoGeneral }) {
       <div className='form-input'>
         <label htmlFor='firstName'>First Name: </label>
         {/* prettier-ignore */}
-        <input type='text' name='firstName' id='firstName' placeholder='Jane' value={infoGeneral.firstName} onChange={ e => setInfoGeneral({...infoGeneral, firstName: e.target.value}) } required/>
+        <input type='text' name='firstName' id='firstName' placeholder='Jane' value={infoGeneral.firstName} onChange={ e => setInfoGeneral({...infoGeneral, firstName: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='lastName'>Last Name: </label>
         {/* prettier-ignore */}
-        <input type='text' name='lastName' id='lastName' placeholder='Doe' value={infoGeneral.lastName} onChange={ e => setInfoGeneral({...infoGeneral, lastName: e.target.value}) } required/>
+        <input type='text' name='lastName' id='lastName' placeholder='Doe' value={infoGeneral.lastName} onChange={ e => setInfoGeneral({...infoGeneral, lastName: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='address'>Address: </label>
         {/* prettier-ignore */}
-        <input type='text' name='address' id='address' placeholder='1234 Magical Dr.' value={infoGeneral.address} onChange={ e => setInfoGeneral({...infoGeneral, address: e.target.value}) } required />
+        <input type='text' name='address' id='address' placeholder='1234 Magical Dr.' value={infoGeneral.address} onChange={ e => setInfoGeneral({...infoGeneral, address: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='phoneNumber'>Phone Number: </label>
         {/* prettier-ignore */}
-        <input type='text' name='phoneNumber' id='phoneNumber' placeholder='1234567890' value={infoGeneral.phoneNumber} onChange={ e => setInfoGeneral({...infoGeneral, phoneNumber: e.target.value}) } required />
+        <input type='text' name='phoneNumber' id='phoneNumber' placeholder='1234567890' value={infoGeneral.phoneNumber} onChange={ e => setInfoGeneral({...infoGeneral, phoneNumber: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='email'>E-mail: </label>
         {/* prettier-ignore */}
-        <input type='text' name='email' id='email' placeholder='janedoe@example.com' value={infoGeneral.email} onChange={ e => setInfoGeneral({...infoGeneral, email: e.target.value}) }  required />
+        <input type='text' name='email' id='email' placeholder='janedoe@example.com' value={infoGeneral.email} onChange={ e => setInfoGeneral({...infoGeneral, email: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='description'>Description: </label>
         {/* prettier-ignore */}
-        <textarea name="description" id="description" cols="30" rows="3" placeholder="Very hard worker, baller" value={infoGeneral.description} onChange={ e => setInfoGeneral({...infoGeneral, description: e.target.value}) } required></textarea>
+        <textarea name="description" id="description" cols="30" rows="3" placeholder="Very hard worker, baller" value={infoGeneral.description} onChange={ e => setInfoGeneral({...infoGeneral, description: e.target.value}) } ></textarea>
       </div>
     </section>
   );
--- src/components/CVCreator/units/ExperienceUnit.js
+++ src/components/CVCreator/units/ExperienceUnit.js
@@ -6,32 +6,32 @@ export default function ExperienceUnit({ infoExperience, setInfoExperience }) {
       <div className='form-input'>
         <label htmlFor='company'>Company: </label>
         {/* prettier-ignore */}
-        <input type='text' name='company' id='company' placeholder='Ziggs Inc. / Self-Employed / etc.' value={infoExperience.company} onChange={ e => setInfoExperience({...infoExperience, company: e.target.value}) } required/>
+        <input type='text' name='company' id='company' placeholder='Ziggs Inc. / Self-Employed / etc.' value={infoExperience.company} onChange={ e => setInfoExperience({...infoExperience, company: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='companyCity'>City: </label>
         {/* prettier-ignore */}
-        <input type='text' name='companyCity' id='companyCity' placeholder='Piltover / Remote / etc.' value={infoExperience.companyCity} onChange={ e => setInfoExperience({...infoExperience, companyCity: e.target.value}) } required />
+        <input type='text' name='companyCity' id='companyCity' placeholder='Piltover / Remote / etc.' value={infoExperience.companyCity} onChange={ e => setInfoExperience({...infoExperience, companyCity: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='jobTitle'>Title: </label>
         {/* prettier-ignore */}
-        <input type='text' name='jobTitle' id='jobTitle' placeholder='Small Explosives Specialist' value={infoExperience.jobTitle} onChange={ e => setInfoExperience({...infoExperience, jobTitle: e.target.value}) } required/>
+        <input type='text' name='jobTitle' id='jobTitle' placeholder='Small Explosives Specialist' value={infoExperience.jobTitle} onChange={ e => setInfoExperience({...infoExperience, jobTitle: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='jobDescription'>Description: </label>
         {/* prettier-ignore */}
-        <input type='text' name='jobDescription' id='jobDescription' placeholder='Handling and overseeing manufacturing of small explosives, field testing, and comitting small crime' required/>
+        <input type='text' name='jobDescription' id='jobDescription' placeholder='Handling and overseeing manufacturing of small explosives, field testing, and comitting small crime'/>
       </div>
       <div className='form-input'>
         <label htmlFor='jobYearStart'>Year started: </label>
         {/* prettier-ignore */}
-        <input type='text' name='jobYearStart' id='jobYearStart' placeholder='2023' value={infoExperience.jobYearStart} onChange={ e => setInfoExperience({...infoExperience, jobYearStart: e.target.value}) } required />
+        <input type='text' name='jobYearStart' id='jobYearStart' placeholder='2023' value={infoExperience.jobYearStart} onChange={ e => setInfoExperience({...infoExperience, jobYearStart: e.target.value}) } />
       </div>
       <div className='form-input'>
         <label htmlFor='jobYearEnd'>Year ended: </label>
         {/* prettier-ignore */}
-        <input type='text' name='jobYearEnd' id='jobYearEnd' placeholder='2025 (or write "current")' value={infoExperience.jobYearEnd} onChange={ e => setInfoExperience({...infoExperience, jobYearEnd: e.target.value}) } required />
+        <input type='text' name='jobYearEnd' id='jobYearEnd' placeholder='2025 (or write "current")' value={infoExperience.jobYearEnd} onChange={ e => setInfoExperience({...infoExperience, jobYearEnd: e.target.value}) } />
       </div>
     </div>
   );
