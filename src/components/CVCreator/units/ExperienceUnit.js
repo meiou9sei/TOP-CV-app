3525e14e398390a0a67ee249db4519e8b2125d09
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-20 15:46:38 -0500

    🚑 forgot to change ExperienceUnit.js along with EducationUnit.js

--- src/components/CVCreator/units/ExperienceUnit.js
+++ src/components/CVCreator/units/ExperienceUnit.js
@@ -1,37 +1,53 @@
 import React from "react";
 
-export default function ExperienceUnit({ infoExperience, setInfoExperience }) {
+export default function ExperienceUnit({
+  infoUnit,
+  infoExperience,
+  setInfoExperience,
+}) {
+  function inputValueUpdater(e, keyUpdating) {
+    setInfoExperience(
+      [...infoExperience].map((obj) => {
+        if (obj.id === infoUnit.id) {
+          return {
+            ...infoUnit,
+            [keyUpdating]: e.target.value,
+          };
+        } else return obj;
+      })
+    );
+  }
   return (
     <div className='unit-experience'>
       <div className='form-input'>
         <label htmlFor='company'>Company: </label>
         {/* prettier-ignore */}
-        <input type='text' name='company' id='company' placeholder='Ziggs Inc. / Self-Employed / etc.' value={infoExperience.company} onChange={ e => setInfoExperience({...infoExperience, company: e.target.value}) } />
+        <input type='text' name='company' id='company' placeholder='Ziggs Inc. / Self-Employed / etc.' value={infoUnit.company} onChange={ e => inputValueUpdater(e, 'company') } />
       </div>
       <div className='form-input'>
         <label htmlFor='companyCity'>City: </label>
         {/* prettier-ignore */}
-        <input type='text' name='companyCity' id='companyCity' placeholder='Piltover / Remote / etc.' value={infoExperience.companyCity} onChange={ e => setInfoExperience({...infoExperience, companyCity: e.target.value}) } />
+        <input type='text' name='companyCity' id='companyCity' placeholder='Piltover / Remote / etc.' value={infoUnit.companyCity} onChange={ e => inputValueUpdater(e, 'companyCity') } />
       </div>
       <div className='form-input'>
         <label htmlFor='jobTitle'>Title: </label>
         {/* prettier-ignore */}
-        <input type='text' name='jobTitle' id='jobTitle' placeholder='Small Explosives Specialist' value={infoExperience.jobTitle} onChange={ e => setInfoExperience({...infoExperience, jobTitle: e.target.value}) } />
+        <input type='text' name='jobTitle' id='jobTitle' placeholder='Small Explosives Specialist' value={infoUnit.jobTitle} onChange={ e => inputValueUpdater(e, 'jobTitle') } />
       </div>
       <div className='form-input'>
         <label htmlFor='jobDescription'>Description: </label>
         {/* prettier-ignore */}
-        <input type='text' name='jobDescription' id='jobDescription' placeholder='Handling and overseeing manufacturing of small explosives, field testing, and comitting small crime'/>
+        <input type='text' name='jobDescription' id='jobDescription' placeholder='Handling and overseeing manufacturing of small explosives, field testing, and comitting small crime' value={infoUnit.jobDescription} onChange={ e => inputValueUpdater(e, 'jobDescription') } />
       </div>
       <div className='form-input'>
         <label htmlFor='jobYearStart'>Year started: </label>
         {/* prettier-ignore */}
-        <input type='text' name='jobYearStart' id='jobYearStart' placeholder='2023' value={infoExperience.jobYearStart} onChange={ e => setInfoExperience({...infoExperience, jobYearStart: e.target.value}) } />
+        <input type='text' name='jobYearStart' id='jobYearStart' placeholder='2023' value={infoUnit.jobYearStart} onChange={ e => inputValueUpdater(e, 'jobYearStart') } />
       </div>
       <div className='form-input'>
         <label htmlFor='jobYearEnd'>Year ended: </label>
         {/* prettier-ignore */}
-        <input type='text' name='jobYearEnd' id='jobYearEnd' placeholder='2025 (or write "current")' value={infoExperience.jobYearEnd} onChange={ e => setInfoExperience({...infoExperience, jobYearEnd: e.target.value}) } />
+        <input type='text' name='jobYearEnd' id='jobYearEnd' placeholder='2025 (or write "current")' value={infoUnit.jobYearEnd} onChange={ e => inputValueUpdater(e, 'jobYearEnd') } />
       </div>
     </div>
   );
